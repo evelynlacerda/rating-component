520d1b1db76d0a26bf6ce1e8879d4201f98c28c1
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-07-31 18:38:36 -0300

    :sparkles: feat: added javascript sheet

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,40 @@
-// START SCRIPT FOR RATING COMPONENT
-// STOP AT MIN35:34
+const buttons = document.querySelectorAll('.buttons button');
+// SELECT ALL BUTTONS OF CLASS BUTTONS
+const form = document.querySelector('#form');
+const cardVote = document.querySelector('.card');
+const cardThanks = document.querySelector('.thank-you');
+const numbSelection = document.querySelector('#select');
+
+function removeSelected () {
+    // REMOVE CLASS SELECTED OF BUTTONS WHEN CLICK IN OTHER BUTTON
+    buttons.forEach((button) => {
+        button.classList.remove('selected');
+    })
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const buttonSelect = document.querySelector('.selected');
+
+        if (buttonSelect) removeSelected();
+
+        button.classList.add('selected');
+    })
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // PREVENT DEFAULT EVENT FORM SUBMIT
+
+    const hasSelectedOption = document.querySelector('.selected');
+
+    if (!hasSelectedOption) return;
+    // RETURN FORM SUBMIT FOR NOT SELECTED OPTION
+
+    const selectedOption = hasSelectedOption.textContent;
+
+    cardVote.classList.remove('active');
+    cardThanks.classList.add('active');
+    numbSelection.innerText = `You selected ${selectedOption} out of 5`;
+    // CHANGE TEXT CARD VOTE FOR CARD THANKS
+})
